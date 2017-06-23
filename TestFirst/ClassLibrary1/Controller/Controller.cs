6d2e888b81f5d8a7977c75ffd21dcb79b358b13f
RobinRitter <robinritter29@gmail.com>
2017-06-23 13:57:33 +0200

    Unbekannte Änderungen

---
+++ TestFirst/ClassLibrary1/Controller/Controller.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+
+namespace SteamBibliothek
+{
+    public class Controller
+    {
+        public void Spielhinzufügen(string Titel, string ErstellDatum, string Zuletztgespielt, string Kategorie, string Publisher, int USK)
+        {
+            XmlDocument xdoc = new XmlDocument();
+            xdoc.Load("SpielListe.xml");
+            XmlNode newGameNode = xdoc.CreateElement("Spiel");
+            XmlNode TitelNode = xdoc.CreateElement("Titel");
+            XmlNode ErstelldatumNode = xdoc.CreateElement("ErstellDatum");
+            XmlNode ZuletztgespieltNode = xdoc.CreateElement("Zuletzt_gespielt");
+            XmlNode KategorieNode = xdoc.CreateElement("Kategorie");
+            XmlNode PublisherNode = xdoc.CreateElement("Publisher");
+            XmlNode USKNode = xdoc.CreateElement("USK");
+        } 
+
+        public void Spielentfernen(string Titel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Spielstarten(string Titel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Datenladen()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Datenschreiben()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TestFirst/ClassLibrary1/Model/Spiel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamBibliothek.Model
+{
+    class Spiel
+    {
+        public string Titel
+        {
+            get;
+            set;
+        }
+        public DateTime InstallationsDatum
+        {
+            get;
+            set;
+        }
+        public DateTime? zuletzt_gespielt
+        {
+            get;
+            set;
+        }
+        public string Kategorie
+        {
+            get;
+            set;
+        }
+        public string Publisher
+        {
+            get;
+            set;
+        }
+        public int USK
+        {
+            get;
+            set;
+        }
+    }
+}
